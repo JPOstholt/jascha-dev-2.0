17eab8ecc438360da85a7b89726d859b71711197
Jascha Ostholt <jascha@elvah.de>
2023-05-15 10:44:41 +0200

    Added arrows and circle to contact nav item

--- src/components/navigation/NavigationItem.vue
+++ src/components/navigation/NavigationItem.vue
@@ -8,6 +8,7 @@ const props = defineProps<{
 
 const link = ref(null)
 
+// pulsate if active
 onMounted(() => {
   ScrollTrigger.create({
     start: 'top center',
@@ -25,8 +26,23 @@ onMounted(() => {
   <a
     ref="link"
     :href="`#${$rt(id)}`"
-    class="jo_text_base leading-tight lg:font-normal tracking-widest max-w-[5rem]"
+    class="jo_text_base leading-tight lg:font-normal tracking-widest max-w-[5rem] relative"
   >
-    {{ $rt(text) }}
+    <span>
+      {{ $rt(text) }}
+    </span>
+    <template v-if="$rt(id) === $t('nav[2].id')">
+      <span
+        class="absolute -top-2 -left-[20%] w-[140%] h-full border border-jo_dark rounded-1/2"
+      />
+      <span
+        v-for="_, index in Array(4)"
+        :key="`nav_arrow_${index}`"
+        class="block w-4 h-4 absolute top-full leading-none pl-0.5"
+        :style="`left: ${index * 40 - 20}%; transform: translateY(${Math.pow(index - 1.5, 2) * -4}px) rotate(${Math.pow(index - 1.5, 3) * -10}deg)`"
+      >
+        ↑
+      </span>
+    </template>
   </a>
 </template>
