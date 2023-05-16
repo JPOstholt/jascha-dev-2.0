463febe72be36a8b4e8d20fa6f197ec2b58f201b
Jascha Ostholt <jascha@elvah.de>
2023-05-16 10:56:03 +0200

    components.d.ts updated

--- components.d.ts
+++ components.d.ts
@@ -9,23 +9,15 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    EbzPanorama: typeof import('./src/components/panorama/EbzPanorama.vue')['default']
-    EbzPanorama2: typeof import('./src/components/EbzPanorama.vue')['default']
-    EbzPanoramaSlide: typeof import('./src/components/panorama/EbzPanoramaSlide.vue')['default']
     JoAccordeon: typeof import('./src/components/JoAccordeon.vue')['default']
     JoBerlinTimer: typeof import('./src/components/JoBerlinTimer.vue')['default']
     JoCursor: typeof import('./src/components/JoCursor.vue')['default']
-    JoDivider: typeof import('./src/components/JoDivider.vue')['default']
-    JoHero: typeof import('./src/components/JoHero.vue')['default']
-    JoHeroHeader: typeof import('./src/components/JoHeroHeader.vue')['default']
     JoHeroHeading: typeof import('./src/components/JoHeroHeading.vue')['default']
     JoImprint: typeof import('./src/components/JoImprint/JoImprint.vue')['default']
     JoImprintContent: typeof import('./src/components/JoImprint/JoImprintContent.vue')['default']
     JoImprintPrivacy: typeof import('./src/components/JoImprint/JoImprintPrivacy.vue')['default']
     JoNavigation: typeof import('./src/components/navigation/JoNavigation.vue')['default']
     NavigationItem: typeof import('./src/components/navigation/NavigationItem.vue')['default']
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
     SectionAbout: typeof import('./src/components/section/SectionAbout.vue')['default']
     SectionContact: typeof import('./src/components/section/SectionContact.vue')['default']
     SectionFooter: typeof import('./src/components/section/SectionFooter.vue')['default']
