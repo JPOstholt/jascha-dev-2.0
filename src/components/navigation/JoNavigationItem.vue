27bdc22b72e74c3f0e975d63be2e246db98713d7
Jascha Ostholt <jascha@elvah.de>
2023-05-22 21:28:09 +0200

    Added hover animation to nav items / refactored contact circle and arrow

--- src/components/navigation/JoNavigationItem.vue
+++ src/components/navigation/JoNavigationItem.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ScrollTrigger } from 'gsap/all'
+import { ScrollTrigger, SplitText, gsap } from 'gsap/all'
 
 const props = defineProps<{
   id: string
@@ -7,6 +7,7 @@ const props = defineProps<{
 }>()
 
 const link = ref(null)
+const linkText = ref(null)
 
 // pulsate if active
 onMounted(() => {
@@ -20,36 +21,62 @@ onMounted(() => {
     },
   })
 })
+
+// hover
+let chars: Element[]
+onMounted(() => {
+  // adding the !block class makes ensures the linebreak in recent projects
+  const split = new SplitText(linkText.value, { type: 'words,chars', wordsClass: '!block' })
+  chars = split.chars
+})
+
+let hoverAnimation: gsap.core.Tween
+const mouseEnter = () => {
+  if (hoverAnimation) {
+    hoverAnimation.play()
+    return
+  }
+
+  hoverAnimation = gsap.to(chars, {
+    fontFamily: 'Italiana,serif',
+    marginRight: '4px',
+    duration: 0.2,
+    stagger: 0.05,
+  })
+}
+
+const mouseLeave = () => {
+  hoverAnimation!.reverse()
+}
 </script>
 
 <template>
   <a
     ref="link"
     :href="`#${$rt(id)}`"
-    class="jo_text_base leading-tight lg:font-normal tracking-widest max-w-[5rem] relative"
+    class="jo_text_base leading-tight lg:font-normal tracking-widest relative"
+    @mouseenter="mouseEnter()" @mouseleave="mouseLeave()"
   >
-    <span>
+    <span ref="linkText">
       {{ $rt(text) }}
     </span>
     <template v-if="$rt(id) === $t('nav[2].id')">
       <span
-        class="absolute -top-2 -left-[20%] w-[140%] h-full border border-jo_dark rounded-1/2"
-        lg="w-[150%] -top-3"
-        xl="w-[165%] -top-3.5"
-      />
-      <span
-        class="flex justify-between relative
-          top-[40%] w-[140%] -left-[20%]"
-        lg="w-[160%] -left-[24%]"
-        xl="w-[170%] -left-[22%]"
+        class="absolute block border border-jo_dark rounded-1/2
+        -left-[18%] w-[130%] -top-[16%] h-[90%]"
       >
         <span
-          v-for="_, index in Array(4)"
-          :key="`nav_arrow_${index}`"
-          class="block w-4 h-4 top-full leading-none pl-0.5 font-display"
-          :style="`transform: translateY(${Math.pow(index - 1.5, 2) * -4}px) rotate(${Math.pow(index - 1.5, 3) * -10}deg)`"
+          class="flex justify-between relative
+            top-[120%] w-[110%] -ml-[5%]"
         >
-          ↑
+          <span
+            v-for="_, index in Array(4)"
+            :key="`nav_arrow_${index}`"
+            class="block w-4 h-4 top-full leading-none pl-0.5 font-display"
+            :style="`transform: translateY(${Math.pow(index - 1.5, 2) * -30}%) rotate(${Math.pow(index - 1.5, 3) * -10}deg)`"
+          >
+            ↑
+          </span>
         </span>
       </span>
     </template>
